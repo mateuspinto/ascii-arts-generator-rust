caa14ab0ad6d9ca2f2ba2c24f5590c00c7d4d2b1
Mateus Pinto da Silva <mateus.p.silva@ufv.br>
2020-07-04 18:57:17 -0300

    now it is rustcly

--- src/art/tests.rs
+++ src/art/tests.rs
@@ -1,18 +1,20 @@
 #[cfg(test)]
 use super::Art;
 #[test]
-fn create_art_from_file() {
-        let _painting = match Art::new_from_file("input/x.txt".to_string()) {
-                Ok(o) => o,
-                Err(e) => panic!(e),
+fn create_art_from_file() -> Result<(), String> {
+        let painting = match Art::new_from_file("input/x.txt".to_string()) {
+                Ok(_o) => Ok(()),
+                Err(e) => Err(e),
         };
+        painting
 }
 
 #[test]
-fn more_chars_than_painting_len_throw_error() {
+fn more_chars_than_painting_len_throw_error() -> Result<(), String> {
         let painting = Art::new_blank(10, 10);
-        let _painting = match painting.insert_characters_randomly('*', 101) {
-                Ok(o) => panic!(o),
-                Err(e) => e,
+        let painting = match painting.insert_characters_randomly('*', 101) {
+                Ok(_o) => Err("Excepecting error, but runned correctly".to_string()),
+                Err(_e) => Ok(()),
         };
+        painting
 }
